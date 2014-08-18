a299d3cf79d52235b25edf2feb773e8cce3e840a
K Nouroozi <knouroozi@gmail.com>
2014-08-18 13:10:18 -0400

    Initial implementation of the #configure block

--- lib/quandl.rb
+++ lib/quandl.rb
@@ -1,5 +1,31 @@
 require "quandl/version"
 
 module Quandl
-  # Your code goes here...
+  # Enable configuration like:
+  #   Quandl.configure do |q|
+  #     ...
+  #   end
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
+
+  class Configuration
+    attr_accessor :auth_token, :api_version
+
+    def initialize
+      @api_version ||= 1
+    end
+  end
 end
