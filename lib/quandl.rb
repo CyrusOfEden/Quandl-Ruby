0bad2e54d16bb43c7b554cc79280188d33418dda
K Nouroozi <knouroozi@gmail.com>
2014-08-18 22:15:23 -0400

    Added Quandl::Favorites and Quandl request to the main lib file.

--- lib/quandl.rb
+++ lib/quandl.rb
@@ -3,27 +3,6 @@ require 'quandl/configuration'
 require 'quandl/dataset'
 require 'quandl/metadata'
 require 'quandl/search'
+require 'quandl/favorites'
+require 'quandl/request'
 require 'open-uri'
-
-module Quandl
-  API_URI = 'http://www.quandl.com/api/'
-
-  def self.build_uri(params)
-    path = [configuration.api_version, 'datasets']
-    if configuration.auth_token
-      params[:auth_token] = configuration.auth_token
-    end
-    unless params[:query]
-      path << params.delete(:source)
-      path << params.delete(:table)
-    end
-    path = path.join('/') + '.' + (params[:options][:format] || 'json')
-    URI(API_URI + path).tap do |uri|
-      uri.query = URI.encode_www_form(params[:options])
-    end
-  end
-
-  def self.get(params = {})
-    open(build_uri(params)).read
-  end
-end
