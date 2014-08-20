c3d94d7ca179e7effffbbeae2070ac86583be260
K Nouroozi <knouroozi@gmail.com>
2014-08-19 21:53:31 -0400

    Quandl now parses the API response (if it's in JSON or CSV)

---
+++ lib/quandl/parse.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'csv'
+
+module Quandl
+
+  CSV::Converters[:blank_to_nil] = Proc.new do |field|
+    field && field.empty? ? nil : field
+  end
+
+  def self.parse(data, format)
+    case format
+    when :json
+      JSON.parse(data, {
+        symbolize_names: true
+      })
+    when :csv
+      CSV.parse(data, {
+        headers: true,
+        header_converters: :symbol,
+        converters: [:all, :blank_to_nil]
+      }).map(&:to_hash)
+    else data
+    end
+  end
+end
